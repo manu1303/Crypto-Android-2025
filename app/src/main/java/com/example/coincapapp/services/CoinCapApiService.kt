057e5947d7ea96cef911ad8dc9ac7be7bba75b02
manu1303 <manuco1303@hotmail.com>
2025-04-10 21:13:30 -0500

    TALLER EN CLASE

--- app/src/main/java/com/example/coincapapp/services/CoinCapApiService.kt
+++ app/src/main/java/com/example/coincapapp/services/CoinCapApiService.kt
@@ -11,7 +11,7 @@ class CoinCapApiService @Inject constructor(
     private val client: HttpClient
 ) {
     suspend fun getAssets(): AssetsResponse {
-        val response: HttpResponse = client.get(urlString = "https://rest.coincap.io/v3/assets?apiKey=bf4f44a4c3f977ca96ecdf26b12e3bae0cff39bd3b613bdb8b21a2128be0a5a0")
+        val response: HttpResponse = client.get(urlString = "https://rest.coincap.io/v3/assets?apiKey=e8489470057ceafffeb5afa036063c4d06d8bfed98f0355021e7e3f02e4c6607")
         return response.body()
     }
 }
